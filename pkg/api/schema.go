0cd793f8bf767a3dc1d250e119d2c407f08868df
GeorgiyKuzora <g.kuzora@korona.net>
2025-02-09 15:43:25 +0300

    Add new schema

--- pkg/api/schema.go
+++ pkg/api/schema.go
@@ -2,16 +2,37 @@ package api
 
 import "time"
 
-type request struct {
-	id int
-	card_id int
-	merchant string
-	amount int
-	timestamp time.Time
+type UserCredentials struct {
+	Username string `json:"username" validate:"required,min=3,max=100"`
+	Password string `json:"password" validate:"required,min=8,max=200"`
 }
 
-type response struct {
-	id int
-	request_id int
-	result string
+type Token struct {
+	Token string `json:"token" validate:"required"`
+}
+
+// Transaction.Type enum
+const (
+	Deposit  = "deposit"
+	Withdraw = "withdraw"
+)
+
+type Transaction struct {
+	Username string `json:"username" validate:"required,min=3,max=100"`
+	Amount int  `json:"amount" validate:"required,gt=0"`
+	Type string `json:"type" validate:"required,type"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+type ReportRequest struct {
+	Username string `json:"username" validate:"required,min=3,max=100"`
+	Start time.Time `json:"start"`
+	End time.Time `json:"end"`
+}
+
+type Report struct {
+	Username string `json:"username" validate:"required,min=3,max=100"`
+	Start time.Time `json:"start"`
+	End time.Time `json:"end"`
+	Transactions []Transaction `json:"transactions"`
 }
